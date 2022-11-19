7d1237dfae3da0177d43af3a1665d4437f8828b1
VictorKokos <Travel.ru15@gmail.com>
2022-11-19 10:44:11 +0300

    2

--- Lab3/Matrix.cs
+++ Lab3/Matrix.cs
@@ -6,22 +6,109 @@ using System.Threading.Tasks;
 
 namespace Lab3
 {
+
+
     internal class Matrix
     {
-      
 
-        // поле матрицы
+        // поля матрицы и её размера
         public int[,] matrix;
         public int size = 0;
-        // Конструктор матрицы без параметров
-       public Matrix()
+        public string name;
+        //перегрузка оператора +
+        public static Matrix operator +(Matrix obj1, Matrix obj2)
         {
-            Console.WriteLine("Создание матрицы");
+            Matrix sum = new Matrix("Sum", obj1.size);
+            sum.matrix = new int[obj1.size, obj1.size];
+            for (int i = 0; i < obj1.size; i++)
+            {
+                for (int j = 0; j < obj1.size; j++)
+                {
+                    sum.matrix[i, j] = obj1.matrix[i, j] + obj2.matrix[i, j];
+                }
+            }
+           
+            return sum;
+        }
+        //обнуление всех элементов матрицы
+        public static Matrix operator -( Matrix obj)
+        {
+            Console.WriteLine($"Обнуление всех элементов матрицы {obj.name}");
+            for (int i = 0; i < obj.size; i++)
+            {
+                for (int j = 0; j < obj.size; j++)
+                {
+                    obj.matrix[i, j] = 0;
+                }
+            }
+            
+
+            return obj;
+        }
+        //Сравнение матриц по нулевому столбцу
+        public static bool operator ==(Matrix obj1, Matrix obj2)
+        {
+            bool areZeroColumnEquals = true;
+            for(int i = 0; i < obj1.size; i++)
+            {
+                if (obj1.matrix[i, 0] != obj2.matrix[i,0])
+                {
+                    areZeroColumnEquals = false;
+                }
+            }
+            
+           
+            return areZeroColumnEquals;
+        }
+        public static bool operator !=(Matrix obj1, Matrix obj2)
+        {
+            bool areZeroColumnEquals = true;
+            for (int i = 0; i < obj1.size; i++)
+            {
+                if (obj1.matrix[i, 0] != obj2.matrix[i,0])
+                {
+                    areZeroColumnEquals = false;
+                }
+            }
+
+
+            return !areZeroColumnEquals;
+        }
+        //Количество отрицательных элементов в матрице
+        public static explicit operator int(Matrix obj)
+        {
+            int countMinus = 0;
+            for (int i = 0; i < obj.size; i++)
+            {
+                for (int j = 0; j < obj.size; j++)
+                {
+                    if(obj.matrix[i, j] < 0)
+                    {
+                        countMinus++;
+                    }
+                }
+            }
+
+
+            return countMinus;
+        }
+        // Конструктор матрицы без параметров
+        public Matrix()
+        {
+            Console.WriteLine("Создание матрицы \n Назовите свою матрицу");
+            name = Console.ReadLine();
+        }
+        public Matrix(string matrName = "default", int razmer = 0)
+        {
+            size = razmer;
+            Console.WriteLine($"Создание матрицы {matrName}");
+            name = matrName;
+            matrix = new int[size, size];
         }
 
         private void ChooseSize()
         {
-            Console.WriteLine("Введите размер матрицы");
+            Console.WriteLine($"Введите размер матрицы {name}");;
              size = Convert.ToInt32(Console.ReadLine());
             matrix = new int[size, size];
         }
@@ -33,8 +120,16 @@ namespace Lab3
             {
                 for (int j = 0; j < size; j++)
                 {
-                    Console.WriteLine($"Введите {j+1} элемент {i+1}строки");
-                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Введите {j+1} элемент {i+1}строки матрицы {name}");
+                    string element = Console.ReadLine();
+                    if (element == "")
+                    {
+                        matrix[i, j] = 0;
+                    }
+                    else 
+                    {
+                        matrix[i, j] = Convert.ToInt32(element);
+                    }
                 }
             }
             WriteMatrix();
@@ -42,6 +137,7 @@ namespace Lab3
 
         public void WriteMatrix()
         {
+            Console.WriteLine($"Матрица {name}:");
             for (int i = 0; i < matrix.Length / matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(0); j++)
@@ -50,8 +146,24 @@ namespace Lab3
                 }
                 Console.Write("\n");
             }
+
+           
         }
+        //вложенный объект
+       public Production production = new();
+        //вложенный класс
+        public class Developer
+        {
+            public string FIO;
+            public int ID;
+            public int department;
+        }
+
     }
 
-
+    public class Production
+    {
+        public int ID;
+        public string Name;
+    }
 }
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,2 +1,74 @@
-﻿Lab3.Matrix Matrica1 = new Lab3.Matrix();
+﻿/*
+//Создаём, определяем размер, заполняем матрицы 1 и 2
+
+Lab3.Matrix Matrica1 = new();
+
+
 Matrica1.FillMatrix();
+
+Lab3.Matrix Matrica2 = new();
+Matrica2.FillMatrix();
+
+//Создаем матрицу 3 - сумму матриц 1 и 2
+Lab3.Matrix Matrica3 = Matrica1 + Matrica2; 
+Matrica3.WriteMatrix();
+// обнуляем матрицу 3
+Matrica3= -(Matrica3);
+Matrica3.WriteMatrix();
+
+*/
+// матрицы 4 и 5 не равны по первому столбцу
+
+Lab3.Matrix Matrica4 = new("4 матрица", 3);
+Lab3.Matrix Matrica5 = new("5 матрица", 3);
+
+Matrica4.matrix = new int[3, 3] { { 0, 2, 5 }, { 34, -2, -5 }, { 0, 56, 5 } };
+Matrica5.matrix = new int[3, 3] { { 45, 26, 25 }, { 3, 2, 5 }, { 30, 6, 55 } };
+
+if(Matrica4 == Matrica5)
+{
+    Console.WriteLine("Матрицы 4 и 5 равны по первому столбцу");
+}
+else
+{
+    Console.WriteLine("Матрицы 4 и 5 не равны по первому столбцу");
+}
+
+// матрицы 6 и 7 равны по первому столбцу
+Lab3.Matrix Matrica6 = new("6 матрица", 3);
+Lab3.Matrix Matrica7 = new("7 матрица", 3);
+
+Matrica6.matrix = new int[3, 3] { { 0, -2, 5 }, { 4, -2, 0 }, { 0, 0, -25 } };
+Matrica7.matrix = new int[3, 3] { { 0, 2, 5 }, { 4, -5, -5 }, { 0, 26, 5 } };
+
+if (Matrica6 == Matrica7)
+{
+    Console.WriteLine("Матрицы 4 и 5 равны по первому столбцу");
+}
+else
+{
+    Console.WriteLine("Матрицы 6 и 7 не равны по первому столбцу");
+}
+
+//Подсчет количества отрицательных элементов
+Console.WriteLine("Количество отрицательных элементов в 5, 6, 7 матрицах");
+Console.WriteLine((int) Matrica5 +"\n");
+Console.WriteLine((int)Matrica6 + "\n");
+Console.WriteLine((int)Matrica7 + "\n");
+
+
+//Инициализация вложенного объекта
+
+Matrica7.production.ID = 0;
+Matrica7.production.Name = "VkusnoITochka";
+
+Console.WriteLine($"ID= {Matrica7.production.ID} \n Name = {Matrica7.production.Name}" );
+
+
+//Инициализация вложенного класса
+Lab3.Matrix.Developer Sasha = new Lab3.Matrix.Developer();
+Sasha.FIO = "Kuchkov Alexander Sergeevich";
+Sasha.ID = 134;
+Sasha.department = 24;
+
+Console.WriteLine($"\n \n ID= {Sasha.ID} \n FIO = {Sasha.FIO} \n DEP = {Sasha.department}");
