1d31fc04b5be0d15d3ebfa307fd251003bfbdec3
Victor <Travel.ru15@gmail.com>
2023-01-21 23:05:09 +0300

    8Lab

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -19,3 +19,43 @@ programmer.Mutated += programmer.Sortirovka;
 programmer.Mutated += programmer.DisplayAll;
 programmer.Replace(1, "abc");
 
+programmer.Mutated -= programmer.DisplayAll;
+programmer.Mutated += programmer.ClearList;
+programmer.Mutated += programmer.DisplayAll;
+programmer.Replace(2, "xxx");
+
+Programmer programmer2 = new Programmer();
+
+programmer2.AddItem("First");
+programmer2.AddItem("Second");
+programmer2.AddItem("Third");
+
+
+
+programmer2.Deleted += programmer2.LeftAndRight;
+
+programmer2.DeleteItem(1);
+
+
+//Task2
+
+
+StringProcessing String1= new StringProcessing();
+String1.str = "Ах, как красиво!";
+String1.DelPunctuation();
+String1.Insert(2,"ахахах" );
+String1.ToUpperCase();
+String1.FirstWord();
+String1.Pad();
+Console.WriteLine(String1.str);
+
+String1.action += String1.DelPunctuation;
+String1.action += String1.ToUpperCase;
+String1.action += String1.FirstWord;
+String1.func += String1.Insert;
+String1.str = "Боже, какая шутка!";
+
+String1.action();
+String1.func(4, "ВОООО");
+Console.WriteLine(String1.str);
+
--- Lab8/Programmer.cs
+++ Lab8/Programmer.cs
@@ -22,7 +22,7 @@ namespace Lab8
        public void AddItem(string str) => list.Add(str);
         public void DeleteItem(int num)
         {
-            Deleted?.Invoke($"Удалён элемент {list[num]} ");
+            Deleted?.Invoke(list[num]);
             list.RemoveAt(num);
            
         }
@@ -41,15 +41,65 @@ namespace Lab8
         public void DisplayMessage(string message) => Console.WriteLine(message);
         public void DisplayAll()
         {
-            foreach(var item in list)
-            Console.WriteLine(item);
+            if (list.Count > 0)
+            {
+                foreach (var item in list)
+                    Console.WriteLine(item);
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            }
         }
         public void Sortirovka()
         {
             list.Sort();
         }
+        public void ClearList()
+        {
+            list.Clear();
+        }
+
+        public void LeftAndRight(string str)
+        {
+            Console.WriteLine($"слева: {list[list.IndexOf(str) - 1]}," +
+                $"Справа : {list[list.IndexOf(str) + 1]}  ");
+        }
 
     }
 
    
+
+
+
+    public class StringProcessing
+    {
+        public string str;
+        public Action action;
+        public Func<int, string, string> func;
+       public void DelPunctuation ()
+        {
+            var sb = new StringBuilder();
+
+            foreach (char c in str)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+
+            str = sb.ToString();
+        }
+        public string Insert(int num, string inserted)
+        {
+            str = str.Insert(num, inserted);
+            return str;
+        }
+        public void ToUpperCase() =>
+          str = str.ToUpper();
+
+        public void FirstWord () =>
+          str = str.Split(' ')[0];
+        public void Pad() =>
+          str = str.PadLeft(10,'7');
+    }
 }
