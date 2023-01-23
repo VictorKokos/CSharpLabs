f61057143063525e173a673fdb224bcebfd269e7
VictorKokos <Travel.ru15@gmail.com>
2023-01-23 20:39:38 +0300

    Lab9 Task2

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -31,3 +31,66 @@ images.ShowSet();
 
 images.Search(2);
 images.Search(1);
+
+
+// Task2
+
+
+var count = new List<int> { 2,3,5,6,2,6,23,76,2345,7,325,979654};
+
+LinkedList<int> numbers = new LinkedList<int>(count);
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+
+Console.WriteLine("Введите количество удаляемых элементов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    numbers.RemoveFirst();
+}
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}
+LinkedListNode<int> node;
+numbers.AddLast(500);
+for (node = numbers.First; node != null; node = node.Next)
+{
+    if (node.Value == 500)
+    {
+        numbers.AddAfter(node, 300);
+     }
+}
+for (node = numbers.First; node != null; node = node.Next)
+{
+    if (node.Value == 300)
+    {
+        numbers.AddBefore(node, 800);
+    }
+}
+
+numbers.AddFirst(9);
+numbers.AddLast(8);
+Console.WriteLine("Вывод:");
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+
+List<int> numbersList = new List<int>(numbers);
+Console.WriteLine("Вывод списка:");
+foreach (int number in numbersList)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("Введите число, которое вы ищете");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(numbersList.Find(number => number == a));
