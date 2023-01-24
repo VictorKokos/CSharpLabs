4eee535f9ee79f897c0da3427c8ac88c5304ce09
Victor <Travel.ru15@gmail.com>
2023-01-24 17:07:28 +0300

    Lab10 start

---
+++ Lab10/Program.cs
@@ -0,0 +1,7 @@
+﻿
+object[] values = new object[] { "June", "July", "May",
+"December", "January", "February", "April", "March ", "September ",
+    "October ", "November ", "August "  };
+IEnumerable<string> strings = values.OfType<string>().Where(i => i.Length == 4); 
+Console.WriteLine(strings);
+// извлекаем все строки, которые начинаются на A
