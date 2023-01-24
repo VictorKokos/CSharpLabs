77fb7329a2ffc31b7a675cdddac49452a6da49ae
VictorKokos <Travel.ru15@gmail.com>
2023-01-25 01:08:45 +0300

    Lab10

---
+++ Lab10/BoolMatrix.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal partial class BoolMatrix
+    {
+        private static int kol { get; set; }
+        private static int PrivateCalls { get; set; } = 0;
+        public bool[,] matrix;
+        private static readonly int standartSize = 3;
+        private const int constructorCount = 5;
+        public int MatrixSize;
+        public BoolMatrix()
+        {
+            kol++;
+            Console.WriteLine("Создание объекта Matrix");
+        }
+        public BoolMatrix(int size)
+        {
+            Random rnd = new Random();
+            kol++;
+            Console.WriteLine("Создание объекта Matrix");
+            
+            matrix = new bool[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                
+                    matrix[i, j] = Convert.ToBoolean(rnd.Next(0, 2));
+                }
+            MatrixSize = size;
+        }
+        public BoolMatrix(string s1 = "000", string s2 = "000", string s3 = "000")
+        {
+            kol++;
+            Console.WriteLine("Создание объекта Matrix");
+            bool b1 = Convert.ToBoolean(Convert.ToInt32(s1[0] - '0'));
+            bool b2 = Convert.ToBoolean(Convert.ToInt32(s1[1] - '0'));
+            bool b3 = Convert.ToBoolean(Convert.ToInt32(s1[2] - '0'));
+
+            bool b4 = Convert.ToBoolean(Convert.ToInt32(s2[0] - '0'));
+            bool b5 = Convert.ToBoolean(Convert.ToInt32(s2[1] - '0'));
+            bool b6 = Convert.ToBoolean(Convert.ToInt32(s2[2] - '0'));
+
+            bool b7 = Convert.ToBoolean(Convert.ToInt32(s3[0] - '0'));
+            bool b8 = Convert.ToBoolean(Convert.ToInt32(s3[1] - '0'));
+            bool b9 = Convert.ToBoolean(Convert.ToInt32(s3[2] - '0'));
+            matrix = new bool[3, 3] { { b1, b2, b3 }, { b4, b5, b6 }, { b7, b8, b9 } };
+        }
+
+        static BoolMatrix()
+        {
+            kol = 0;
+        }
+
+        private BoolMatrix(bool a)
+        {
+            PrivateCalls++;
+        }
+
+        public bool[,] SumMatrix(bool[,] m1, bool[,] m2)
+        {
+            bool[,] m3 = new bool[3, 3];
+            for (int i = 0; i < m1.Length / m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m1.GetLength(0); j++)
+                {
+                    if ((m1[i, j] == true && m2[i, j] == true) ||
+                        (m1[i, j] == true && m2[i, j] == false) ||
+                       (m1[i, j] == false && m2[i, j] == true))
+                    {
+                        m3[i, j] = true;
+                    }
+                    else if (m1[i, j] == false && m2[i, j] == false)
+                    {
+                        m3[i, j] = false;
+                    }
+                }
+            }
+            return m3;
+        }
+        public bool[,] MultipltyMatrix(bool[,] m1, bool[,] m2)
+        {
+            bool[,] m3 = new bool[3, 3];
+            for (int i = 0; i < m1.Length / m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m1.GetLength(0); j++)
+                {
+                    if (m1[i, j] == true && m2[i, j] == true)
+                    {
+                        m3[i, j] = true;
+                    }
+                    else if ((m1[i, j] == false && m2[i, j] == false) ||
+                        (m1[i, j] == true && m2[i, j] == false) ||
+                       (m1[i, j] == false && m2[i, j] == true))
+                    {
+                        m3[i, j] = false;
+                    }
+                }
+            }
+            return m3;
+        }
+        public bool[,] Inversion(bool[,] m1)
+        {
+            bool[,] m3 = new bool[3, 3];
+            for (int i = 0; i < m1.Length / m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m1.GetLength(0); j++)
+                {
+                    if (m1[i, j] == true)
+                    {
+                        m3[i, j] = false;
+                    }
+                    else
+                    {
+                        m3[i, j] = true;
+                    }
+
+                }
+            }
+            return m3;
+        }
+        public bool areEqualSymbols()
+        {
+            int countInStr = 0;
+            int compareTo = -10;
+            bool areSame = false;
+            for (int j = 0; j < matrix.Length / matrix.GetLength(0); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(0); k++)
+                {
+                    if (matrix[j, k] == true)
+                    {
+                        countInStr++;
+                    }
+                }
+                if (j > 0)
+                {
+                    if (compareTo != countInStr)
+                    {
+                        areSame = false; break;
+                    }
+                }
+                compareTo = countInStr;
+                areSame = true;
+                countInStr = 0;
+            }
+            if (areSame)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int Count1()
+        {
+            int count = 0;
+
+            for (int i = 0; i < this.matrix.Length / this.matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.matrix.GetLength(0); j++)
+                {
+                    if (this.matrix[i, j] == true)
+                    {
+                        count++;
+                    }
+
+                }
+            }
+            return count;
+        }
+
+        public void WriteMatrix()
+        {
+            for (int i = 0; i < matrix.Length / matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("//////////////////////");
+        }
+
+        public void RevertStr(ref string strToRevert)
+        {
+            char[] chars = strToRevert.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = chars[i] == '0' ? '1' : '0';
+            }
+            strToRevert = string.Join("", chars); ;
+        }
+
+    }
+
+}
+
--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,7 +1,133 @@
 ﻿
+using Lab10;
+using System.Linq;
+
 object[] values = new object[] { "June", "July", "May",
-"December", "January", "February", "April", "March ", "September ",
-    "October ", "November ", "August "  };
-IEnumerable<string> strings = values.OfType<string>().Where(i => i.Length == 4); 
-Console.WriteLine(strings);
-// извлекаем все строки, которые начинаются на A
+"December", "January", "February", "April", "March", "September",
+"October", "November", "August"  };
+Console.WriteLine("Выберите количество символов");
+int kol = Convert.ToInt32(Console.ReadLine());
+IEnumerable<string> strings = values.OfType<string>().Where(i => i.Length == kol);
+
+foreach(string s in strings)
+Console.WriteLine(s);
+
+Console.WriteLine("//");
+IEnumerable<string> strings2 = values.OfType<string>().Where(i => 
+i == "June" || i == "July" || i == "August" ||
+i == "January" || i == "February" || i == "December");
+
+foreach (string s in strings2)
+    Console.WriteLine(s);
+Console.WriteLine("//");
+
+
+IEnumerable<string> strings3 = values.OfType<string>().OrderBy(i => i);
+
+
+
+foreach (string s in strings3)
+    Console.WriteLine(s);
+Console.WriteLine("//");
+
+
+IEnumerable<string> strings4 = values.OfType<string>().Where(i => i.Contains("u"));
+
+foreach (string s in strings4)
+    Console.WriteLine(s);
+Console.WriteLine("//");
+
+
+IEnumerable<string> strings5 = values.OfType<string>().Where(i => i.Length >=4);
+
+foreach (string s in strings5)
+    Console.WriteLine(s);
+Console.WriteLine("//");
+
+//Task2
+
+
+
+
+BoolMatrix bool1 = new BoolMatrix(2);
+BoolMatrix bool2 = new BoolMatrix(2);
+BoolMatrix bool3 = new BoolMatrix(2);
+BoolMatrix bool4 = new BoolMatrix(2);
+BoolMatrix bool5 = new BoolMatrix(2);
+BoolMatrix bool6 = new BoolMatrix(3);
+BoolMatrix bool7 = new BoolMatrix(4);
+BoolMatrix bool8 = new BoolMatrix(3);
+BoolMatrix bool9 = new BoolMatrix(3);
+BoolMatrix bool10 = new BoolMatrix(3);
+
+
+List<BoolMatrix> bools = new List<BoolMatrix>();
+bools.Add(bool1);
+bools.Add(bool2);
+bools.Add(bool3);
+bools.Add(bool4);
+bools.Add(bool5);
+bools.Add(bool6);
+bools.Add(bool7);
+bools.Add(bool8);
+bools.Add(bool9);
+bools.Add(bool10);
+
+for(int i = 0; i<10; i++)
+{
+    bools[i].WriteMatrix();
+}
+
+
+IEnumerable<BoolMatrix> boolsEnum1 = bools.OrderBy(i => i.Count1());
+BoolMatrix maxOne = boolsEnum1.Last();
+BoolMatrix minOne = boolsEnum1.First();
+Console.WriteLine("MAX");
+maxOne.WriteMatrix();
+Console.WriteLine("MIN");
+minOne.WriteMatrix();
+
+IEnumerable<BoolMatrix> boolsEnum2 = bools.Where(i => i.areEqualSymbols() == true);
+Console.WriteLine("SAME IN 1 STRING");
+foreach (var s in boolsEnum2)
+{
+    s.WriteMatrix();
+}
+Console.WriteLine("MAX SIZE");
+IEnumerable<BoolMatrix> boolsEnum3 = bools.OrderBy(i => i.MatrixSize);
+BoolMatrix maxSize = boolsEnum3.Last();
+maxSize.WriteMatrix();
+
+
+Console.WriteLine("Выберите размер");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int kolvo = bools.Where(i => i.MatrixSize == size).Count();
+
+Console.WriteLine($"Количество матриц заданного размера: {kolvo}") ;
+
+Console.WriteLine("Упорядоченный список матриц по количеству единиц ");
+foreach (var s in boolsEnum1)
+{
+    s.WriteMatrix();
+}
+
+float average = (float)bools.Where(m => m.MatrixSize == 3).Reverse().Distinct().Skip(1).Average(m => m.Count1());
+Console.WriteLine(average);
+
+
+BoolMatrix bool11 = new BoolMatrix(2);
+BoolMatrix bool12 = new BoolMatrix(2);
+BoolMatrix bool13 = new BoolMatrix(2);
+BoolMatrix bool14 = new BoolMatrix(2);
+BoolMatrix bool15 = new BoolMatrix(2);
+
+List<BoolMatrix> bools2 = new List<BoolMatrix>();
+bools2.Add(bool11);
+bools2.Add(bool12);
+
+
+var join = bools.Join(bools2, c => c.MatrixSize, d => d.MatrixSize, (c,d) => d.MatrixSize);
+
+foreach (var s in join)
+    Console.WriteLine(s);
