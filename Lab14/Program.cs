42b85078f3a50bec72a847b708a62da6ae53a222
VictorKokos <Travel.ru15@gmail.com>
2023-01-31 05:48:58 +0300

    Lab14

---
+++ Lab14/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading;
+using System.Diagnostics;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lab14;
+
+DateTime current = DateTime.Now;
+    foreach (Process process in Process.GetProcesses())
+    {
+    try
+    {
+        Console.WriteLine($"ID: {process.Id} " +
+            $" Имя: {process.ProcessName}, Приоритет {process.BasePriority}, " +
+            $"Время запуска: {process.StartTime}, Текущее состояние: {process.Responding}" +
+            $"сколько всего времени использовал процессор: {current - process.StartTime}");
+    }
+    catch 
+    {
+        Console.WriteLine("ex");
+    }
+}
+
+ 
+AppDomain domain = AppDomain.CurrentDomain;
+Console.WriteLine($"Name: {domain.FriendlyName}");
+Console.WriteLine($"Base Directory: {domain.BaseDirectory}");
+Console.WriteLine();
+
+Assembly[] assemblies = domain.GetAssemblies();
+foreach (Assembly asm in assemblies)
+    Console.WriteLine(asm.GetName().Name);
+
+
+var asmb = Assembly.LoadFrom("HelloWorld.dll");
+
+Console.WriteLine(asmb.GetName());
+
+
+Lab14.Threads threads = new Lab14.Threads();
+threads.n = Convert.ToInt32(Console.ReadLine());
+ThreadStart threadDelegate = new ThreadStart(threads.CalculateSimple);
+Thread myThread1 = new Thread(threadDelegate);
+myThread1.Start();
+
+Console.WriteLine($"{myThread1.IsAlive},{myThread1.Priority},{myThread1.ThreadState}   ");
+myThread1.Interrupt();
+
+
+ThreadStart threadDelegate2 = new ThreadStart(threads.Chet);
+Thread myThread2 = new Thread(threadDelegate2);
+
+ThreadStart threadDelegate3 = new ThreadStart(threads.NeChet);
+Thread myThread3 = new Thread(threadDelegate3);
+Thread.Sleep(1000);
+
+myThread2.Priority = ThreadPriority.Highest;
+
+Semaphore sem = new Semaphore(1, 1);
+
+myThread3.Start();
+
+myThread2.Start();
+
+
+
+Console.ReadLine();
+int num = 0;
+// устанавливаем метод обратного вызова
+TimerCallback tm = new TimerCallback(threads.Repeating);
+// создаем таймер
+Timer timer = new Timer(tm, num, 0, 2000);
+Console.ReadLine();
+
+
+
+
+
---
+++ Lab14/Threads.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab14
+{
+
+    public class Threads
+    {
+        static Mutex mutex = new Mutex();
+        object locker = new object();
+
+        public int n = 0;
+
+        public void CalculateSimple()
+        {
+
+            for (int i = 1; i < n; i++)
+            {
+                bool isSimple = false;
+                int m = 2;
+                while (m < i)
+                {
+
+                    if (i % m == 0)
+                    {
+                        isSimple = true;
+                        break;
+                    }
+                    m++;
+                }
+                if (!isSimple)
+                {
+                    Console.Write($"{i}, ");
+                    using (StreamWriter sw = new StreamWriter(@"D:\Work\CSharpLabs\Lab14\Simple.txt", true))
+                    {
+                        sw.Write($"{i}, ");
+                    }
+                }
+            }
+        }
+        public void Chet()
+        {
+            //lock(locker)
+            //{ 
+            //mutex.WaitOne();
+
+            for (int i = 1; i < n; i++)
+            {
+
+                Thread.Sleep(30);
+                if (i % 2 == 0)
+                {
+                    Console.Write($"{i}, ");
+                    FileStreamOptions fileStreamOptions = new FileStreamOptions();
+                    fileStreamOptions.Share = FileShare.Write;
+                    fileStreamOptions.Mode = FileMode.Append;
+                    fileStreamOptions.Access = FileAccess.Write;
+                    using (StreamWriter sw = new StreamWriter(@"D:\Work\CSharpLabs\Lab14\ChetNechet.txt", fileStreamOptions))
+                    {
+                        sw.Write($"{i}, ");
+                    }
+                }
+
+            }
+            //}
+            //mutex.ReleaseMutex();
+        }
+        public void NeChet()
+        {
+
+            //lock (locker)
+            //{
+            //mutex.WaitOne();
+            for (int i = 1; i < n; i++)
+            {
+                Thread.Sleep(100);
+
+                if (i % 2 == 1)
+                {
+                    Console.Write($"{i}, ");
+                    FileStreamOptions fileStreamOptions = new FileStreamOptions();
+                    fileStreamOptions.Share = FileShare.Write;
+                    fileStreamOptions.Mode = FileMode.Append;
+                    fileStreamOptions.Access = FileAccess.Write;
+                    using (StreamWriter sw = new StreamWriter(@"D:\Work\CSharpLabs\Lab14\ChetNechet.txt", fileStreamOptions))
+                    {
+                        sw.Write($"{i}, ");
+                    }
+                }
+
+            }
+            //}
+            //mutex.ReleaseMutex();
+        }
+        public void Repeating(object obj)
+        {
+                Random random = new Random();
+                int r = random.Next(1, 1000);
+                Console.Write($"RAND {r} ");
+        }
+    }
+}
+    
+     
